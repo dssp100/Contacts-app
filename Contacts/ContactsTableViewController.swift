d9230d7c8c4e09091fbd5d2f5db86c9f767a3f70
dssp100 <dssp@freenet.de>
2017-01-01 07:35:01 -0400

    Added an edit button function that lets you delete or move your contact. Project works but its not as described in the Thinkful lesson. My version however is simpler and works better as it combines the deleting and moving and adds a done button to finalise the operation.

--- Contacts/ContactsTableViewController.swift
+++ Contacts/ContactsTableViewController.swift
@@ -16,6 +16,12 @@ class ContactsTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       self.navigationItem.leftBarButtonItem = self.editButtonItem;
+        
+        func toggleEdit() {
+            tableView.setEditing(!tableView.isEditing, animated: true)
+        }
+        
         let jenny = Contact(phoneNumber: "867 - 5309")
         let rich = Contact(name: "Rich", phoneNumber: "888-888-8888")
         let mindy = Contact(name: "Mindy")
@@ -61,13 +67,15 @@ class ContactsTableViewController: UITableViewController {
             return cell
         }    
     
-    /*
+    
      // Override to support conditional editing of the table view.
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the specified item to be editable.
-     return true
-     }
-     */
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.contacts.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
     
     /*
      // Override to support editing the table view.
@@ -81,13 +89,14 @@ class ContactsTableViewController: UITableViewController {
      }
      */
     
-    /*
+
      // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
+override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+    let contactMoving = contacts.remove(at: fromIndexPath.row)
+    contacts.insert(contactMoving, at: to.row)
+}
+
+
     /*
      // Override to support conditional rearranging of the table view.
      override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
